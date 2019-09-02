4667a31c20ae350e17586b8fe2034aa2c635a38d
Alumno <Alumno@PC22>
2019-09-02 09:53:06 -0500

    curiosidades de combo box, agregar comidas

---
+++ ComboBox-practica/ComboBox-practica/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ComboBox_practica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboalimentos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BoxFood = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboalimentos
+            // 
+            this.comboalimentos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboalimentos.FormattingEnabled = true;
+            this.comboalimentos.Items.AddRange(new object[] {
+            "Hue",
+            "vos"});
+            this.comboalimentos.Location = new System.Drawing.Point(259, 12);
+            this.comboalimentos.Name = "comboalimentos";
+            this.comboalimentos.Size = new System.Drawing.Size(457, 39);
+            this.comboalimentos.TabIndex = 0;
+            this.comboalimentos.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // BoxFood
+            // 
+            this.BoxFood.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BoxFood.Location = new System.Drawing.Point(259, 289);
+            this.BoxFood.Name = "BoxFood";
+            this.BoxFood.Size = new System.Drawing.Size(203, 38);
+            this.BoxFood.TabIndex = 2;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnagregar.Location = new System.Drawing.Point(564, 252);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(152, 75);
+            this.btnagregar.TabIndex = 3;
+            this.btnagregar.Text = "agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(630, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.BoxFood);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboalimentos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboalimentos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox BoxFood;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ComboBox-practica/ComboBox-practica/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox_practica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboalimentos.Items.Add("pazza");
+            comboalimentos.Items.Add("abzta");
+                
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indice = comboalimentos.SelectedIndex;
+            label1.Text = indice.ToString();
+            label2.Text = comboalimentos.Items[indice].ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            comboalimentos.Items.Add(BoxFood.Text);
+        }
+    }
+}
